90f717bc5152183cbc9450423bab3ce3e4bfb087
Antes <ciul060@gmail.com>
2021-11-30 01:29:07 -0700

    added form dates with components

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ConfigService } from '../config/config.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service:ConfigService) { }
 
   ngOnInit(): void {
+    this.service.getAllDates().subscribe(data => console.log(data));
   }
+
+
+
 
 }
